397534a480e75a43ef8acc521aaea76cc07086cd
riccardo <riccardo.pischtschan@gmail.com>
2023-03-31 11:09:08 +0200

    init

---
+++ app/src/main/java/com/example/spottifly/StartFragment.kt
@@ -0,0 +1,32 @@
+package com.example.spottifly
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.spottifly.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.home_Fragment)
+        },5000)
+    }
+}
